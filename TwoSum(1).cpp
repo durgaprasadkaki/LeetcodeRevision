1e7c047cd286839f7bd3e1982ec72dd9fe940311
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-02-13 21:56:54 +0530

    Added Optimal

--- TwoSum(1).cpp
+++ TwoSum(1).cpp
@@ -45,3 +45,23 @@ public:
      
     }
 };
+//Optimal Approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     int n=nums.size();
+     int left=0, right=n-1;
+     sort(nums.begin(), nums.end());
+     while(left<right){
+        int sum = nums[left]+nums[right];
+        if(sum==target){
+            return {left, right};
+        }else if(sum<target){
+            left++;
+        }
+        else right++;
+     }
+        return {-1, -1};
+     
+    }
+};
