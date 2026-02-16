88093fdee98af1e4ecde0aa5b13a4580cd225ba3
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-02-16 22:11:51 +0530

    added

---
+++ ReverseBits(190).cpp
@@ -0,0 +1,39 @@
+/*190. Reverse Bits
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Reverse bits of a given 32 bits signed integer.
+
+ 
+
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+Example 2:
+
+Input: n = 2147483644
+
+Output: 1073741822*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) | (n & 1);
+            n >>= 1;
+        }
+        return result;
+    }
+};
