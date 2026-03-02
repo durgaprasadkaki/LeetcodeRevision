73b1f457ee338a41ad9d03ac98a6b6ed1be9d202
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-03-02 19:00:31 +0530

    Added Count Subarrays Sum equals K

---
+++ CountSubarraysSumEqualsK(560).cpp
@@ -0,0 +1,59 @@
+/*
+560. Subarray Sum Equals K
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int>mpp;
+        mpp[0]=1;
+        int presum=0, cnt=0;
+        for(int i=0; i<nums.size(); i++){
+            presum+=nums[i];
+            int remove =presum-k;
+            cnt+=mpp[remove];
+            mpp[presum]+=1;
+        }
+        return cnt;
+    }
+};
+/*
+int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            int sum=0;
+            for(int j=i; j<n; j++){
+                sum+=nums[j];
+                if(sum==k) cnt ++;
+            }
+        }
+        return cnt;
+    }*/
