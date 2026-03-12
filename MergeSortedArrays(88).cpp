cf6449cb590729a9dade65ea7252de5f7f4d5ee1
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-03-12 18:06:58 +0530

    Added 1st optimal in merge two sorted arrays

--- MergeSortedArrays(88).cpp
+++ MergeSortedArrays(88).cpp
@@ -27,6 +27,30 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
+        int left = m-1;
+        int right = 0;
+
+        while(left >= 0 && right < n){
+            if(nums1[left] > nums2[right]){
+                swap(nums1[left], nums2[right]);
+                left--;
+                right++;
+            }
+            else break;
+        }
+
+        sort(nums1.begin(), nums1.begin()+m);
+        sort(nums2.begin(), nums2.end());
+
+        // copy nums2 into nums1
+        for(int i=0;i<n;i++){
+            nums1[m+i] = nums2[i];
+        }
+    }
+};
+/*
+ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
         vector<int> nums3(m+n);
 
         int left = 0;
@@ -53,5 +77,4 @@ public:
         for(int i = 0; i < m+n; i++){
             nums1[i] = nums3[i];
         }
-    }
-};
+    }*/
