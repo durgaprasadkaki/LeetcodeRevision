e5419fd2d36d6e92aa5adafd4e0286df0ce498a1
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-04-06 22:56:43 +0530

    Added 3Sum Closest

---
+++ Arrays and basics/3SumClosest(16).cpp
@@ -0,0 +1,60 @@
+/*
+16. 3Sum Closest
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums 
+such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - closest)) {
+                    closest = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return target;
+                }
+            }
+        }
+
+        return closest;
+    }
+};
