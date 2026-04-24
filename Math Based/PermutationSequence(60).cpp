933e01f9720227a32d5b22cc7a945faf9267893c
DurgaPrasadKaki <kakisuresh297@gmail.com>
2026-04-24 22:09:40 +0530

    Added Permutation Sequence

---
+++ Math Based/PermutationSequence(60).cpp
@@ -0,0 +1,65 @@
+/*
+60. Permutation Sequence
+Solved
+Hard
+Topics
+premium lock icon
+Companies
+The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+Given n and k, return the kth permutation sequence.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 3
+Output: "213"
+Example 2:
+
+Input: n = 4, k = 9
+Output: "2314"
+Example 3:
+
+Input: n = 3, k = 1
+Output: "123"*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> numbers;
+        int fact = 1;
+        
+        for(int i = 1; i < n; i++) {
+            fact *= i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        
+        k--; 
+        string ans = "";
+        
+        while(true) {
+            int index = k / fact;
+            ans += to_string(numbers[index]);
+            
+            numbers.erase(numbers.begin() + index);
+            
+            if(numbers.size() == 0) break;
+            
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        
+        return ans;
+    }
+};
